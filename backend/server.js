13d3b2ce48a06fe1af3d8cc6faa3c66e4d553027
Ogaraga <raymond.ogaraga@gmail.com>
2024-09-20 23:50:26 +0100

    socket.io

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        }    
+});
+io.on('connection', (socket)=>{
+    console.log('Socket connection is on with socketID: '+ socket.id);
+    socket.on('chat', (data)=>{
+        io.emit('chat', data)
+        });   
+    socket.on('disconnect',(socket)=>console.log('User disconnected'))  
+            
+});
+server.listen(4001, ()=>console.log('App running on port 4001'));
